3537f63c64e8741a8aad3a67584f224e360f654c
Joao Victor Ignacio <joao.ignacio@venturus.org.br>
2020-06-16 08:41:04 -0300

    Added Swagger

---
+++ ChatAPI/Chat.WebAPI/Configurations/SwaggerConfig.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+
+namespace Chat.WebAPI.Configurations
+{
+    public static class SwaggerConfig
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Chat API",
+                    Description = "Chat API Swagger Surface",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "João Victor Ignacio",
+                        Email = "ignaciojvig@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/ignaciojv/")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT",
+                        Url = new Uri("https://github.com/ignaciojvig/ChatAPI/blob/master/LICENSE")
+                    }
+
+                });
+            });
+        }
+
+        public static void UseSwaggerSetup(this IApplicationBuilder app)
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+        }
+    }
+}
--- ChatAPI/Chat.WebAPI/Startup.cs
+++ ChatAPI/Chat.WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Chat.WebAPI.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,7 +26,11 @@ namespace Chat.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // WebAPI Config
             services.AddControllers();
+
+            // Swagger Config
+            services.AddSwaggerConfiguration();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +51,8 @@ namespace Chat.WebAPI
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwaggerSetup();
         }
     }
 }
